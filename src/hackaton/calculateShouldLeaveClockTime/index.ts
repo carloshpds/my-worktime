ff411bee03aefce1bead19774b6ee08eb60d6e06
Marco Souza <marco.souza@mercadolivre.com>
2020-07-31 16:46:35 -0300

    Refactoring code

--- src/hackaton/calculateShouldLeaveClockTime/index.ts
+++ src/hackaton/calculateShouldLeaveClockTime/index.ts
@@ -27,26 +27,30 @@ export default (data: LeaveClockTimeParams): string => {
   let minutesQuantityNecessaryToOut: number;
   let minutesFromTheLastMark: number;
   let breakMinutes: number;
-  let hourToStopWork: string;
 
-  if(data.isMissingPairMark && data.registeredWorkedMinutes > data.journeyTimeInMinutes) {
-    data.marks.pop();
-  }
-
+  Utils.clearWhenMissingMark(data);
   minutesQuantityNecessaryToOut = data.journeyTimeInMinutes - data.registeredWorkedMinutes;
   minutesFromTheLastMark = ClockHelper.convertClockStringToMinutes(data.marks[Utils.getLastIndex(data.marks)].clock);
+  breakMinutes = Utils.calculateBreakMinutes(data);
 
-  breakMinutes = 0;
-  if(data.breakMinutes>60 && data.registeredWorkedMinutes > data.journeyTimeInMinutes && data.isMissingPairMark === false) {
-    breakMinutes = data.breakMinutes - 60;  
-  }
-
-  hourToStopWork = ClockHelper.humanizeMinutesToClock((minutesFromTheLastMark + minutesQuantityNecessaryToOut) - breakMinutes);
-  return hourToStopWork; 
+  return ClockHelper.humanizeMinutesToClock((minutesFromTheLastMark + minutesQuantityNecessaryToOut) - breakMinutes);
 }
 
 export class Utils {
   public static getLastIndex(marks: WorktimeDayMark[]): number {
     return marks.length - 1;
+  }
+
+  public static clearWhenMissingMark(data: LeaveClockTimeParams) {
+    if(data.isMissingPairMark && data.registeredWorkedMinutes > data.journeyTimeInMinutes) {
+      data.marks.pop();
+    }
+  }
+
+  public static calculateBreakMinutes(data: LeaveClockTimeParams): number{
+    if(data.breakMinutes>60 && data.registeredWorkedMinutes > data.journeyTimeInMinutes && data.isMissingPairMark === false) {
+      return data.breakMinutes - 60;  
+    }
+    return 0;
   }
 }
