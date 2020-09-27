d2017f5bbc67261ad0d8174f8f1ea789a5319f7f
Carlos Henrique <carlos.praeiros@mercadolivre.com>
2020-09-27 17:14:43 -0300

    Check Command - Improving docs and add printed result

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 my-worktime
 ==========
-
+See your worktime using terminal
 
 
 [![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
@@ -11,6 +11,7 @@ my-worktime
 
 <!-- toc -->
 * [Install](#install)
+* [Most Common Usage](#most-common-usage)
 * [Usage](#usage)
 * [Commands](#commands)
 <!-- tocstop -->
@@ -25,6 +26,14 @@ You can also add an alias to make your life esier:
 ```
 $ npm install -g mw@npm:my-worktime
 ```
+
+# Most Common Usage
+
+```
+mw check -u 321 -p 123 -s ahgora -c a22
+```
+![my-worktime-check](https://user-images.githubusercontent.com/2482989/94374192-f0597180-00e0-11eb-8fbb-39d67975963d.gif)
+
 
 # Usage
 
@@ -58,12 +67,13 @@ OPTIONS
   -h, --help                     show CLI help
   -j, --journeytime=journeytime  [default: 08:00] Quantidade de horas a serem trabalhadas por dia
   -p, --password=password        (required) Senha do usuário no sistema
-  -s, --system=system            (required) [default: ahgora] Nome do sistema de ponto
+  -s, --system=system            [default: ahgora] Nome do sistema de ponto
   -u, --user=user                (required) ID do usuário no sistema de ponto
 
 EXAMPLES
-  $ worktime check -u 16238 -p 123 -s ahgora -c a382748 -j 08:48
-  $ worktime check -u 16238 -p 123 -c a382748
+  $ worktime check -u 321 -p 123 -c a22
+  $ worktime check -u 321 -p 123 -s ahgora -c a22 -j 08:48
+  $ worktime check -u 321 -p 123 -s ahgora -c a22 -j 08:48 -d 2020-09-23
 ```
 
 _See code: [src/commands/check.ts](https://github.com/carloshpds/my-worktime/blob/v1.0.0/src/commands/check.ts)_
--- src/commands/check.ts
+++ src/commands/check.ts
@@ -5,23 +5,25 @@ import ClockHelper from '../utils/ClockHelper'
 import Ahgora from '../providers/Ahgora'
 import WorktimeProvider from '../providers/WorktimeProvider'
 import * as ora from 'ora'
+import * as chalk from 'chalk'
 
 export default class CheckCommand extends Command {
   static description = 'Checks your worktime'
 
   static examples = [
-    '$ worktime check -u 16238 -p 123 -s ahgora -c a382748 -j 08:48',
-    '$ worktime check -u 16238 -p 123 -c a382748',
+    '$ worktime check -u 321 -p 123 -c a22',
+    '$ worktime check -u 321 -p 123 -s ahgora -c a22 -j 08:48',
+    '$ worktime check -u 321 -p 123 -s ahgora -c a22 -j 08:48 -d 2020-09-23',
   ]
 
   static flags = {
     help: flags.help({char: 'h'}),
     user: flags.string({char: 'u', description: 'ID do usuário no sistema de ponto', required: true}),
     password: flags.string({char: 'p', description: 'Senha do usuário no sistema', required: true}),
-    system: flags.string({char: 's', description: 'Nome do sistema de ponto', required: true, default: 'ahgora'}),
+    system: flags.string({char: 's', description: 'Nome do sistema de ponto', default: 'ahgora'}),
     company: flags.string({char: 'c', description: 'ID da empresa no sistema de ponto', required: true, default: 'ahgora'}),
     date: flags.string({char: 'd', description: 'Data relacionada a consulta de horas no padrão YYYY-MM-DD', default: moment().format('YYYY-MM-DD')}),
-    debug: flags.boolean({char: 'b', description: 'Debug - Exibe mais informações na execução'}),
+    debug: flags.boolean({char: 'b', description: 'Debug - Exibe mais informações na execução', default: false}),
     journeytime: flags.string({char: 'j', description: 'Quantidade de horas a serem trabalhadas por dia', default: '08:00'}),
   }
 
@@ -69,15 +71,34 @@ export default class CheckCommand extends Command {
         loader.text = `Buscando dados no ${worktimeProvider.name}`
 
         const worktimeDayResume: WorktimeDayResume = await worktimeProvider.getWorktimeDayResume()
-        loader.succeed(`Dados encontrados, seu horário de saída ideal é ${worktimeDayResume.shouldLeaveClockTime}`)
-        // console.log(marks.map(mark => mark.clock).join(' '))
-        worktimeDayResume.marks.length && console.table(worktimeDayResume.marks)
+        loader.succeed(`Dados encontrados, seu horário de saída ideal é ${chalk.black.bgGreen(' ' + worktimeDayResume.shouldLeaveClockTime + ' ')}`)
+        if(worktimeDayResume.marks.length){ this.printResult(worktimeDayResume) }
+
       } catch (error) {
-        options.debug && console.error(error)
+        console.error(error)
         loader.fail('Não foi possível calcular. Verifique os parâmetros e tente novamente')
       }
     } else {
       loader.fail('Parece que ainda não suportamos o seu sistema de ponto :(')
     }
+  }
+
+  printResult(worktimeDayResume: WorktimeDayResume){
+    const marksToConsole = worktimeDayResume.marks.map((mark, index) => {
+      const isLastMark = index === worktimeDayResume.marks.length - 1
+      let markOnConsole = chalk.green(mark.clock)
+
+      if(isLastMark && worktimeDayResume.isMissingPairMark){
+        markOnConsole = chalk.yellow(mark.clock) + chalk.gray(' Batida ímpar')
+      }
+
+      return `${markOnConsole}`
+    })
+
+    console.log('')
+    console.log(`🔢 Batidas: ${marksToConsole.join('   ')}`)
+    console.log(`🆗 Horas registradas: ${ClockHelper.humanizeMinutesToClock(worktimeDayResume.registeredWorkedMinutes)}`)
+    console.log(`⏺  Horas trabalhadas até este momento: ${ClockHelper.humanizeMinutesToClock(worktimeDayResume.workedMinutesUntilNow)}`)
+    console.log('')
   }
 }
