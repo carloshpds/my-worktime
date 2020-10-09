5ceec4c084193ae29af8b11db2bf7ed774c4b649
Tavisco <otavio.pinheiro@mercadolivre.com>
2020-10-09 14:50:18 -0300

    Now using strategy pattern to show inquiries

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -9,6 +9,7 @@ import WorktimeProvider from '../providers/WorktimeProvider'
 import ClockHelper from '../utils/ClockHelper'
 import * as moment from 'moment'
 import { MeliBUs } from '../enums/MeliBusinessUnits'
+import {CLIError} from '@oclif/errors'
 
 export default class Setup extends Command {
   static description = 'Setup the correct properties for your company'
@@ -32,72 +33,142 @@ export default class Setup extends Command {
       default: false
     }])
 
-    var secondStepQuestions = [] as inquirer.QuestionCollection[]
-
-    if (firstStepInquirer.isMeli) {
-      secondStepQuestions.push({
-        name: 'isDefaultMeliWorktime',
-        message: 'Você tem o expediente padrão do Meli (08:48)?',
-        type: 'confirm',
-        default: true
-      },
-      {
-        name: 'whichMeli',
-        message: 'Para qual BU você trabalha?',
-        type: 'list',
-        choices: [{name: MeliBUs.MARKETPLACE.name, value: MeliBUs.MARKETPLACE.code}, {name: MeliBUs.MERCADO_ENVIOS.name, value: MeliBUs.MERCADO_ENVIOS.code}]
-      })
-    } else {
-      secondStepQuestions.push({
-        name: 'provider',
-        message: 'Selecione um sistema de pontos',
-        type: 'list',
-        choices: [{name: AHGORA}],
-      },
-      {
-        name: 'journeytime',
-        message: 'Selecione o período da sua jornada de trabalho',
-        type: 'list',
-        choices: [{name: '08:00'}, {name: '08:48'}, {name: '06:00'}, {name: OTHER}],
-      },
-      {
-        name: 'ahgoraCompanyId',
-        message: 'Digite a companyId do Ahgora da sua empresa',
-        type: 'input'
-      })
+    function meliFluxSecondStep(): inquirer.QuestionCollection[] {
+      return [
+        {
+          name: 'whichMeli',
+          message: 'Para qual BU você trabalha?',
+          type: 'list',
+          choices: [{name: MeliBUs.MARKETPLACE.name, value: MeliBUs.MARKETPLACE.code}, {name: MeliBUs.MERCADO_ENVIOS.name, value: MeliBUs.MERCADO_ENVIOS.code}]
+        },
+        {
+          name: 'isDefaultMeliWorktime',
+          message: 'Você tem o expediente padrão do Meli (08:48)?',
+          type: 'confirm',
+          default: true
+        }
+      ].concat(inquireAhgoraCredentials()) as inquirer.QuestionCollection[]
     }
 
-    let secondStepInquirer: any = await inquirer.prompt(secondStepQuestions)
-    var thirdStepQuestions = [] as inquirer.QuestionCollection[]
-
-    if (secondStepInquirer.journeytime === OTHER || secondStepInquirer.isDefaultMeliWorktime === false) {
-      thirdStepQuestions.push({
-        name: 'customJourneytime',
-        message: 'Especifique sua jornada de trabalho no formato HH:MM :',
-        type: 'input'
-      })
+    function otherCompaniesFluxSecondStep(): inquirer.QuestionCollection[] {
+      return [
+        {
+          name: 'provider',
+          message: 'Selecione um sistema de pontos',
+          type: 'list',
+          choices: [{name: AHGORA}],
+        },
+        {
+          name: 'journeytime',
+          message: 'Selecione o período da sua jornada de trabalho',
+          type: 'list',
+          choices: [{name: '08:00'}, {name: '08:48'}, {name: '06:00'}, {name: OTHER}],
+        },
+        {
+          name: 'ahgoraCompanyId',
+          message: 'Digite a companyId do Ahgora da sua empresa',
+          type: 'input'
+        }
+      ] as inquirer.QuestionCollection[]
     }
 
-    if (firstStepInquirer.isMeli === true || secondStepInquirer.provider === AHGORA) {
-      thirdStepQuestions.push({
-        name: 'ahgoraUsername',
-        message: 'Digite seu nome de usuário do Ahgora:',
-        type: 'input'
+    function inquireAhgoraCredentials(): any {
+      return [
+        {
+          name: 'ahgoraUsername',
+          message: 'Digite seu nome de usuário do Ahgora:',
+          type: 'input'
+        },
+        {
+          name: 'ahgoraPassword',
+          message: 'Digite sua senha do Ahgora:',
+          type: 'password',
+          mask: '*'
+        }
+      ] as inquirer.QuestionCollection[]
+    }
+
+    function inquireCustomJourneyTime(): any {
+      return [
+        {
+          name: 'customJourneytime',
+          message: 'Especifique sua jornada de trabalho no formato HH:MM :',
+          type: 'input'
+        }
+      ] as inquirer.QuestionCollection[]
+    }
+ 
+    function meliFluxThirdStep(secondStepInquirer: any): inquirer.QuestionCollection[] {
+      if (secondStepInquirer.isDefaultMeliWorktime === false) {
+        return inquireCustomJourneyTime()
+      }
+      return [] as inquirer.QuestionCollection[]
+    }
+
+    function otherCompaniesFluxThirdStep(secondStepInquirer: any): inquirer.QuestionCollection[] {
+      var inquiries = [] as inquirer.QuestionCollection[]
+
+      if (secondStepInquirer.journeytime === OTHER) {
+        inquiries = inquiries.concat(inquireCustomJourneyTime())
+      }
+
+      if (secondStepInquirer.provider === AHGORA) {
+        inquiries = inquiries.concat(inquireAhgoraCredentials())
+      }
+      
+      return inquiries
+    }
+
+    function meliFluxGenerateOptions(secondStepInquirer: any ,thirdStepInquirer: any): WorktimeProviderOptions {
+      return {
+        userId: secondStepInquirer.ahgoraUsername,
+        password: secondStepInquirer.ahgoraPassword,
+        systemId: 'ahgora',
+        companyId: secondStepInquirer.whichMeli,
+        date: moment().format('YYYY-MM-DD'),
+        debug: false,
+        journeyTime: secondStepInquirer.isDefaultMeliWorktime? '08:48' : thirdStepInquirer.customJourneytime,
+        momentDate: moment()
+      }
+    }
+
+    function otherCompaniesGenerateOptions(secondStepInquirer: any ,thirdStepInquirer: any): WorktimeProviderOptions {
+      if (secondStepInquirer.provider === AHGORA) {
+        return {
+          userId: thirdStepInquirer.ahgoraUsername,
+          password: thirdStepInquirer.ahgoraPassword,
+          systemId: 'ahgora',
+          companyId: secondStepInquirer.ahgoraCompanyId,
+          date: moment().format('YYYY-MM-DD'),
+          debug: false,
+          journeyTime: secondStepInquirer.journeytime !== OTHER? secondStepInquirer.journeytime : thirdStepInquirer.customJourneytime,
+          momentDate: moment()
+        }
+      } else {
+        throw new CLIError("Somente o Ahgora é suportado no momento!")
+      }
+    }
+    
+    const fluxs = {
+      true: {
+        secondStep: meliFluxSecondStep,
+        thirdStep: meliFluxThirdStep,
+        generateOptions: meliFluxGenerateOptions
       },
-      {
-        name: 'ahgoraPassword',
-        message: 'Digite sua senha do Ahgora:',
-        type: 'password',
-        mask: '*'
-      })
+      false: {
+        secondStep: otherCompaniesFluxSecondStep,
+        thirdStep: otherCompaniesFluxThirdStep,
+        generateOptions: otherCompaniesGenerateOptions
+      }
     }
 
-    let thirdStepInquirer: any = await inquirer.prompt(thirdStepQuestions)
+    let secondStepInquirer: any = await inquirer.prompt(fluxs[firstStepInquirer.isMeli].secondStep())
+    let thirdStepInquirer: any = await inquirer.prompt(fluxs[firstStepInquirer.isMeli].thirdStep(secondStepInquirer))
 
     const loader = ora('Iniciando...').start()
 
     try {
-      const worktimeProvider: WorktimeProvider = new Ahgora(this.generateOptions(firstStepInquirer, secondStepInquirer, thirdStepInquirer))
+      const worktimeProvider: WorktimeProvider = new Ahgora(fluxs[firstStepInquirer.isMeli].generateOptions(secondStepInquirer, thirdStepInquirer))
       loader.text = `Buscando dados no ${worktimeProvider.name}`
 
       const worktimeDayResume: WorktimeDayResume = await worktimeProvider.getWorktimeDayResume()
