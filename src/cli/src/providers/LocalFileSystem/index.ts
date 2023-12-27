1be171477184c3a2dbe13c52c937c26711768f6d
Carlos Henrique <carloshpds@gmail.com>
2023-12-27 04:06:33 -0300

    hit command: fix multiple hits and saving

--- src/cli/src/providers/LocalFileSystem/index.ts
+++ src/cli/src/providers/LocalFileSystem/index.ts
@@ -1,3 +1,5 @@
+import { ux } from "@oclif/core";
+
 import LocalConfigManager from "../../tools/LocalConfigManager/index.ts";
 import WorktimeProvider from "../WorktimeProvider.ts";
 import { WorktimeDayMark, WorktimeDayResume } from "../types.ts";
@@ -5,17 +7,18 @@ import { WorktimeDayMark, WorktimeDayResume } from "../types.ts";
 export default class LocalFileSystemProvider extends WorktimeProvider {
   async addMarksByClocksString(marksString: string): Promise<WorktimeDayResume> {
     const registeredMarks = await this.getDateMarks()
-    const newMarks = marksString.split(' ').map(clock => {
+    const newMarks = marksString.split(',').map(clock => {
 
       if (this.options.debug) {
-        console.log(`Adding ${clock} on ${this.options.date}`);
+        ux.info(`Adicionando ${ux.colorize('blue', clock)} em ${ux.colorize('blue', this.options.date)}`)
       }
 
-      return { clock }
+      return { clock: clock.trim() }
     })
 
     const marks = [...registeredMarks, ...newMarks]
     const worktimeDayResume: WorktimeDayResume = this.calculateWorktimeDayResume(marks)
+    LocalConfigManager.saveSettings({ custom: { marks: { [this.options.date]: marks } } })
     return worktimeDayResume
   }
 
